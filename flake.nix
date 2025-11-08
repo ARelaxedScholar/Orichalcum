09cb97db3ce7af898169f36b134571bf83ecbb14
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2025-11-07 20:10:29 -0500

    feat: Added a flake.nix and cargo2nix support to Orichalcum

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A flake.nix to allow easy development with the Orichalcum framework for Nix afficionados.";
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.12";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [cargo2nix.overlays.default];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.85.0";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in rec {
+        # This section defines the "finished product"
+        packages = {
+          orichalcum = (rustPkgs.workspace.orichalcum {});
+          default = packages.orichalcum;
+        };
+
+        # This section defines the "developer workshop"
+        devShells.default = pkgs.mkShell {
+          # Pulls in all build dependencies (rustc, cargo)
+          # from your 'orichalcum' package.
+          inputsFrom = [ packages.orichalcum ];
+
+          nativeBuildInputs = [
+            pkgs.rust-analyzer
+            pkgs.cargo-edit
+            pkgs.rustfmt
+          ];
+        };
+      }
+    );
+}
